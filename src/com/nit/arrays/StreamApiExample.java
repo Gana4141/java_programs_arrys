e8706fa5c4da8ac4efd4065450120de2f5c20109
gana4141 <ganeshpagar288@gmail.com>
2026-09-22 00:12:19 +0530

    update the project

--- src/com/nit/arrays/StreamApiExample.java
+++ src/com/nit/arrays/StreamApiExample.java
@@ -11,21 +11,21 @@ public class StreamApiExample {
 
         List<Integer> numbers = Arrays.asList(10, 5, 20, 15, 30, 25, 40);
 
-        // Find even numbers
+       
         List<Integer> evenNumbers = numbers.stream()
                 .filter(n -> n % 2 == 0)
                 .collect(Collectors.toList());
 
         System.out.println("Even Numbers: " + evenNumbers);
 
-        // Sort numbers
+      
         List<Integer> sortedNumbers = numbers.stream()
                 .sorted()
                 .collect(Collectors.toList());
 
         System.out.println("Sorted Numbers: " + sortedNumbers);
 
-        // Calculate sum
+       
         int sum = numbers.stream()
                 .mapToInt(Integer::intValue)
                 .sum();
