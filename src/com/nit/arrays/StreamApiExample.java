0fa47c780451e7839ccf3e54e7d7fdc191f3181b
gana4141 <ganeshpagar288@gmail.com>
2026-09-22 00:05:40 +0530

    "add  new java progarams "

---
+++ src/com/nit/arrays/StreamApiExample.java
@@ -0,0 +1,36 @@
+package com.nit.arrays;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamApiExample {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(10, 5, 20, 15, 30, 25, 40);
+
+        // Find even numbers
+        List<Integer> evenNumbers = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println("Even Numbers: " + evenNumbers);
+
+        // Sort numbers
+        List<Integer> sortedNumbers = numbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println("Sorted Numbers: " + sortedNumbers);
+
+        // Calculate sum
+        int sum = numbers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        System.out.println("Sum: " + sum);
+    }
+}
+
